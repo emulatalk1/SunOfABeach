76e6e78b65720cf6e5ae600b72389fc1c9a7130c
Chúc Bá Hiếu <Spectre@Chucs-MacBook-Pro.local>
2017-10-30 10:36:41 +0700

    make sure no duplicate dates exist in the database.

--- app/src/main/java/com/vnspectre/sunofabeach/data/WeatherDbHelper.java
+++ app/src/main/java/com/vnspectre/sunofabeach/data/WeatherDbHelper.java
@@ -13,7 +13,7 @@ import static com.vnspectre.sunofabeach.data.WeatherContract.*;
 public class WeatherDbHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "weather.db";
-    private static final int DATABASE_VERSION = 2;
+    private static final int DATABASE_VERSION = 3;
 
     public WeatherDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -30,7 +30,8 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
                 + WeatherEntry.COLUMN_HUMIDITY + "REAL NOT NULL, "
                 + WeatherEntry.COLUMN_PRESSURE + "REAL NOT NULL, "
                 + WeatherEntry.COLUMN_WIND_SPEED + "REAL NOT NULL, "
-                + WeatherEntry.COLUMN_DEGREES + "REAL NOT NULL" + ");";
+                + WeatherEntry.COLUMN_DEGREES + "REAL NOT NULL, "
+                + " UNIQUE (" + WeatherEntry.COLUMN_DATE + ") ON CONFLICT REPLACE);";
 
         db.execSQL(SQL_CREATE_WEATHER_TABLE);
     }
