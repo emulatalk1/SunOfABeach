ae58419c50889735a3194be51d7da0df6c7d72ae
Chúc Bá Hiếu <Spectre@Chucs-MacBook-Pro.local>
2017-11-01 14:26:52 +0700

    Edit Preferences ulti

--- app/src/main/java/com/vnspectre/sunofabeach/data/SunOfABeachPreferences.java
+++ app/src/main/java/com/vnspectre/sunofabeach/data/SunOfABeachPreferences.java
@@ -14,23 +14,18 @@ import com.vnspectre.sunofabeach.R;
 public class SunOfABeachPreferences {
 
     /*
-     * Human readable location string, provided by the API.
-     */
-    public static final String PREF_CITY_NAME = "city_name";
-
-    /*
      * In order to uniquely pinpoint the location on the map when we launch the
      * map intent, we store the latitude and longitude.
      */
     public static final String PREF_COORD_LAT = "coord_lat";
     public static final String PREF_COORD_LONG = "coord_long";
 
-    /*
-     * Before we implement methods to return your REAL preference for location,
-     * I use some default values to test.
-     */
-    private static final String DEFAULT_WEATHER_LOCATION = "Việt Nam";
-    private static final double[] DEFAULT_WEATHER_COORDINATES = {21.0333, 105.85};
+//    /*
+//     * Before we implement methods to return your REAL preference for location,
+//     * I use some default values to test.
+//     */
+//    private static final String DEFAULT_WEATHER_LOCATION = "Việt Nam";
+//    private static final double[] DEFAULT_WEATHER_COORDINATES = {21.0333, 105.85};
 
     /**
      * Returns true if the user has selected metric temperature display.
@@ -66,15 +61,15 @@ public class SunOfABeachPreferences {
         return prefs.getString(keyForLocation, defaultLocation);
     }
 
-    private static String getDefaultWeatherLocation() {
-        // TODO need to implement.
-        return DEFAULT_WEATHER_LOCATION;
-    }
-
-    public static double[] getDefaultWeatherCoordinates() {
-        // TODO need to implement.
-        return DEFAULT_WEATHER_COORDINATES;
-    }
+//    private static String getDefaultWeatherLocation() {
+//        // TODO need to implement.
+//        return DEFAULT_WEATHER_LOCATION;
+//    }
+//
+//    public static double[] getDefaultWeatherCoordinates() {
+//        // TODO need to implement.
+//        return DEFAULT_WEATHER_COORDINATES;
+//    }
 
     /**
      * Helper method to handle setting location details in Preferences (city name, latitude,
@@ -92,6 +87,108 @@ public class SunOfABeachPreferences {
 
         editor.putLong(PREF_COORD_LAT, Double.doubleToRawLongBits(lat));
         editor.putLong(PREF_COORD_LONG, Double.doubleToRawLongBits(lon));
+        editor.apply();
+    }
+
+    /**
+     * Resets the location coordinates stores in SharedPreferences.
+     *
+     * @param context Context used to get the SharedPreferences
+     */
+    public static void resetLocationCoordinates(Context context) {
+        SharedPreferences sp = android.preference.PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = sp.edit();
+
+        editor.remove(PREF_COORD_LAT);
+        editor.remove(PREF_COORD_LONG);
+        editor.apply();
+    }
+
+    /**
+     * Returns the location coordinates associated with the location. Note that there is a
+     * possibility that these coordinates may not be set, which results in (0,0) being returned.
+     *
+     * @param context used to access SharedPreferences
+     * @return an array containing the two coordinate values for the user's preferred location
+     */
+    public static double[] getLocationCoordinates(Context context) {
+        SharedPreferences sp = android.preference.PreferenceManager.getDefaultSharedPreferences(context);
+
+        double[] preferredCoordinates = new double[2];
+
+        preferredCoordinates[0] = Double
+                .longBitsToDouble(sp.getLong(PREF_COORD_LAT, Double.doubleToRawLongBits(0.0)));
+        preferredCoordinates[1] = Double
+                .longBitsToDouble(sp.getLong(PREF_COORD_LONG, Double.doubleToRawLongBits(0.0)));
+
+        return preferredCoordinates;
+    }
+
+    /**
+     * Returns true if the latitude and longitude values are available.
+     *
+     * @param context used to get the SharedPreferences
+     * @return true if lat/long are saved in SharedPreferences
+     */
+    public static boolean isLocationLatLonAvailable(Context context) {
+        SharedPreferences sp = android.preference.PreferenceManager.getDefaultSharedPreferences(context);
+
+        boolean spContainLatitude = sp.contains(PREF_COORD_LAT);
+        boolean spContainLongitude = sp.contains(PREF_COORD_LONG);
+
+        boolean spContainBothLatitudeAndLongitude = false;
+        if (spContainLatitude && spContainLongitude) {
+            spContainBothLatitudeAndLongitude = true;
+        }
+
+        return spContainBothLatitudeAndLongitude;
+    }
+
+    /**
+     * Returns the last time that a notification was shown (in UNIX time)
+     *
+     * @param context Used to access SharedPreferences
+     * @return UNIX time of when the last notification was shown
+     */
+    public static long getLastNotificationTimeInMillis(Context context) {
+        /* Key for accessing the time at which Sunshine last displayed a notification */
+        String lastNotificationKey = context.getString(R.string.pref_last_notification);
+
+        /* As usual, we use the default SharedPreferences to access the user's preferences */
+        SharedPreferences sp = android.preference.PreferenceManager.getDefaultSharedPreferences(context);
+
+        long lastNotificationTime = sp.getLong(lastNotificationKey, 0);
+
+        return lastNotificationTime;
+    }
+
+    /**
+     * Returns the elapsed time in milliseconds since the last notification was shown. This is used
+     * as part of our check to see if we should show another notification when the weather is
+     * updated.
+     *
+     * @param context Used to access SharedPreferences as well as use other utility methods
+     * @return Elapsed time in milliseconds since the last notification was shown
+     */
+    public static long getEllapsedTimeSinceLastNotification(Context context) {
+        long lastNotificationTimeMillis =
+                SunOfABeachPreferences.getLastNotificationTimeInMillis(context);
+        long timeSinceLastNotification = System.currentTimeMillis() - lastNotificationTimeMillis;
+        return timeSinceLastNotification;
+    }
+
+    /**
+     * Saves the time that a notification is shown. This will be used to get the ellapsed time
+     * since a notification was shown.
+     *
+     * @param context Used to access SharedPreferences
+     * @param timeOfNotification Time of last notification to save (in UNIX time)
+     */
+    public static void saveLastNotificationTime(Context context, long timeOfNotification) {
+        SharedPreferences sp = android.preference.PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = sp.edit();
+        String lastNotificationKey = context.getString(R.string.pref_last_notification);
+        editor.putLong(lastNotificationKey, timeOfNotification);
         editor.apply();
     }
 }
---
+++ app/src/main/java/com/vnspectre/sunofabeach/utilities/FakeDataUtils.java
@@ -0,0 +1,8 @@
+package com.vnspectre.sunofabeach.utilities;
+
+/**
+ * Created by Spectre on 10/31/17.
+ */
+
+public class FakeDataUtils {
+}
